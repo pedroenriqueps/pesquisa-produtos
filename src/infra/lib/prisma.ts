017d8f2e6dc2ca8c1ed77845a50c0e2082b9a946
pedroenriqueps <pedroeps.18@gmail.com>
2025-01-15 19:08:17 -0300

    feat: modified prisma for production

--- src/infra/lib/prisma.ts
+++ src/infra/lib/prisma.ts
@@ -1,4 +1,17 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient()
 
-export default prisma
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
+const prisma =
+    global.prisma ||
+    new PrismaClient({
+        log: ['query', 'info', 'warn', 'error'],
+    });
+
+if (process.env.NODE_ENV !== "production") {
+    global.prisma = prisma;
+}
+
+export default prisma;
